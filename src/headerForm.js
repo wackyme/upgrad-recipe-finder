8a2d2d080dc2b8e4524a814b324dcc9ab7882ea8
sathvik  gangili <65996488+wackyme@users.noreply.github.com>
2021-07-13 13:37:12 +0530

    updated headerform

--- src/headerForm.js
+++ src/headerForm.js
@@ -1,59 +1,54 @@
 import React, { Component } from "react";
 import "./css/header-form.css";
-import RecipeModel from "./RecipeBody";
+import RecipeModel from "./RecipeModel";
 
 class HeaderForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searched: false,
-            foodItem: "",
-        };
-    }
-
-    getRecipe = (e) => {
-        e.preventDefault();
-
-        var item = document.getElementById("text").value;
-
-        this.setState({
-            searched: true,
-            foodItem: item,
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      searched: false,
+      foodItem: "",
     };
+  }
 
-    render() {
-        return ( <
-            div >
-            <
-            div className = "header-slot" >
-            <
-            h1 className = "heading" > Recipe Finder < /h1> <
-            form className = "input-form" >
-            <
-            input type = "text"
-            placeholder = "Enter the name of the dish"
-            id = "text" /
-            >
-            <
-            input type = "submit"
-            value = "Get Ingredients"
-            onClick = { this.getRecipe }
-            /> <
-            /form> <
-            /div> {
-                this.state.searched ? ( <
-                    RecipeModel foodItem = { this.state.foodItem }
-                    />
-                ) : ( <
-                    h2 className = "text-position" >
-                    Type a Dish Name to search
-                    for its ingredients <
-                    /h2>
-                )
-            } <
-            /div>
-        );
-    }
+  getRecipe = (e) => {
+    e.preventDefault();
+
+    var item = document.getElementById("text").value;
+
+    this.setState({
+      searched: true,
+      foodItem: item,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header-slot">
+          <h1 className="heading">Recipe Finder</h1>
+          <form className="input-form">
+            <input
+              type="text"
+              placeholder="Enter the name of the dish"
+              id="text"
+            />
+            <input
+              type="submit"
+              value="Get Ingredients"
+              onClick={this.getRecipe}
+            />
+          </form>
+        </div>
+        {this.state.searched ? (
+          <RecipeModel foodItem={this.state.foodItem} />
+        ) : (
+          <h2 className="text-position">
+            Type a Dish Name to search for its ingredients
+          </h2>
+        )}
+      </div>
+    );
+  }
 }
 export default HeaderForm;
